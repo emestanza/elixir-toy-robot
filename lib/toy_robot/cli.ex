68035abaf804a299c5cf8a530830bfa481a03034
Enmanuel Mestanza <enmanuel.mestanza@zubale.com>
2022-03-02 13:01:23 -0500

    cli lib script started

---
+++ lib/toy_robot/cli.ex
@@ -0,0 +1,14 @@
+defmodule ToyRobot.CLI do
+  def main([file_name]) do
+    File.stream!(file_name)
+    |> Enum.map(&String.trim/1)
+    |> ToyRobot.CommandInterpreter.interpret()
+    |> ToyRobot.CommandRunner.run()
+  end
+
+  def main([]) do
+    IO.puts "Usage: toy_robot commands.txt"
+  end
+
+
+end
