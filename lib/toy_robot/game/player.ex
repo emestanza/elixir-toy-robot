00e6a9a5f43e04274db01f09d94dfe39424fd8d3
Enmanuel Mestanza <enmanuel.mestanza@zubale.com>
2022-09-11 00:19:40 -0500

    Player module, first tests

---
+++ lib/toy_robot/game/player.ex
@@ -0,0 +1,29 @@
+defmodule ToyRobot.Game.Player do
+  use GenServer
+
+  alias ToyRobot.Robot
+
+  def start(position) do
+    GenServer.start(__MODULE__, position)
+  end
+
+  def init(robot) do
+    {:ok, robot}
+  end
+
+  def report(player) do
+    GenServer.call(player, :report)
+  end
+
+  def move(player) do
+    GenServer.cast(player, :move)
+  end
+
+  def handle_call(:report, _from, robot) do
+    {:reply, robot, robot}
+  end
+
+  def handle_cast(:move, robot) do
+    {:noreply, robot |> Robot.move()}
+  end
+end
