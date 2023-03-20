c8c2df50ee4bf7f872ca85566c9b12c27bdbe04f
Enmanuel Mestanza <enmanuel.mestanza@zubale.com>
2023-03-20 15:06:28 -0500

    code advances

--- lib/toy_robot/game/player.ex
+++ lib/toy_robot/game/player.ex
@@ -3,8 +3,15 @@ defmodule ToyRobot.Game.Player do
 
   alias ToyRobot.{Simulation, Robot}
 
-  def start_link([table: table, position: position, name: name]) do
-    GenServer.start_link(__MODULE__, [table: table, position: position], name: process_name(name))
+  def start_link(registry_id: registry_id, table: table, position: position, name: name) do
+    GenServer.start_link(
+      __MODULE__,
+      [
+        table: table,
+        position: position
+      ],
+      name: process_name(registry_id, name)
+    )
   end
 
   def start(table, position) do
@@ -37,7 +44,7 @@ defmodule ToyRobot.Game.Player do
     {:noreply, new_simulation}
   end
 
-  def process_name(name) do
-    {:via, Registry, {ToyRobot.Game.PlayerRegistry, name}}
+  def process_name(registry_id, name) do
+    {:via, Registry, {registry_id, name}}
   end
 end
